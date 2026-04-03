e534422bb67f6a32b23cfa46a5e39a2eefdaac7e
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-04-03 06:55:52 +0530

    [TASK] : Day 1 completed

---
+++ day-01/Task/script.js
@@ -0,0 +1,3 @@
+console.log("Hello Tapascript");
+console.log("Welcome to 40 days Javascript");
+document.write("Outputs text in HTML")
---
+++ day-01/test-script.js
@@ -0,0 +1,3 @@
+console.log("I am a Test Script");
+
+document.getElementById("someID").innerText = "I am Some ID";
